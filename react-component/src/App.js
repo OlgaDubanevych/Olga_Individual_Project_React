d9833b280a301aa87bb1547097b98b98e6d151f1
OlgaDubanevych <114818674+OlgaDubanevych@users.noreply.github.com>
2023-03-16 23:05:22 -0400

    Navigation Bar

---
+++ react-component/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Toolbar from "./components/Toobar";
+import "./styles.css";
+import SideDrawer from "./components/sidemenu/SideDrawer";
+import BackDrop from "./components/backdrop/BackDrop";
+
+class App extends React.Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState(prevState => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+  backDropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+
+  render() {
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <BackDrop click={this.backDropClickHandler} />;
+    }
+    return (
+      <div style={{ height: "100%" }}>
+        <Toolbar drawerToggleClickHandler={this.drawerToggleClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
+
+        <main style={{ marginTop: "64px" }}>
+          <p>This is the content</p>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
